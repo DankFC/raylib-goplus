ed7fd5ac81e69ac5ab4bb973adefd1f95985176a
Dank <44716232+DankFC@users.noreply.github.com>
2020-05-28 12:57:35 -0700

    Update text_gen.go

--- raylib/text_gen.go
+++ raylib/text_gen.go
@@ -28,31 +28,18 @@ func LoadFont(fileName string) *Font {
 }
 
 //LoadFontEx : Load font from file with extended parameters
-func LoadFontEx(fileName string, fontSize int, charsCount int) (*Font, []int) {
-
-	cCharsCount := C.int(int32(charsCount))
-	cFontChars := C.malloc(C.size_t(cCharsCount) * C.size_t(unsafe.Sizeof(int32(0))))
+func LoadFontEx(fileName string, fontSize int, fontChars *int, charsCount int) *Font {
 	cfileName := C.CString(fileName)
-
+	//cFontChars := C.CString(fontChars)
 	defer C.free(unsafe.Pointer(cfileName))
-	defer C.free(unsafe.Pointer(cFontChars))
-
-	//Read the font resource
-	res := C.LoadFontEx(cfileName, C.int(int32(fontSize)), (*C.int)(unsafe.Pointer(&cFontChars)), cCharsCount)
-
-	//Convert the font chars back to a go array
-	tempFontChars := (*[1 << 29]C.int)(cFontChars)
-	goFontChars := make([]int, charsCount)
-	for i, value := range tempFontChars {
-		goFontChars[i] = int(int32(value))
-	}
-
-	//Create the font object
-	retval := newFontFromPointer(unsafe.Pointer(&res))
-	RegisterUnloadable(retval)
-
-	//Return the result
-	return retval, goFontChars
+	//defer C.free(unsafe.Pointer(cFontChars))
+	cfontSize := (C.int)(fontSize)
+	cfontChars := (*C.int)(unsafe.Pointer(fontChars))
+	ccharsCount := (C.int)(charsCount)
+	ret := C.LoadFontEx(cfileName, cfontSize, cfontChars, ccharsCount)
+	v := newFontFromPointer(unsafe.Pointer(&ret))
+	//RegisterUnloadable(ret)
+	return v
 }
 
 //LoadFontFromImage : Load font from Image (XNA style)
