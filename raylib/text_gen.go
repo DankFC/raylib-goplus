50accd48d807df930462129fd9ad2cf88783cc27
Dank <44716232+DankFC@users.noreply.github.com>
2020-06-05 13:16:11 -0700

    Add files via upload

--- raylib/text_gen.go
+++ raylib/text_gen.go
@@ -28,6 +28,35 @@ func LoadFont(fileName string) *Font {
 }
 
 //LoadFontEx : Load font from file with extended parameters
+/*
+func LoadFontEx(fileName string, fontSize int, charsCount int) (*Font, []int) {
+
+	cCharsCount := C.int(int32(charsCount))
+	cFontChars := C.malloc(C.size_t(cCharsCount) * C.size_t(unsafe.Sizeof(int32(0))))
+	cfileName := C.CString(fileName)
+
+	defer C.free(unsafe.Pointer(cfileName))
+	defer C.free(unsafe.Pointer(cFontChars))
+
+	//Read the font resource
+	res := C.LoadFontEx(cfileName, C.int(int32(fontSize)), (*C.int)(unsafe.Pointer(&cFontChars)), cCharsCount)
+
+	//Convert the font chars back to a go array
+	tempFontChars := (*[1 << 29]C.int)(cFontChars)
+	goFontChars := make([]int, charsCount)
+	for i, value := range tempFontChars {
+		goFontChars[i] = int(int32(value))
+	}
+
+	//Create the font object
+	retval := newFontFromPointer(unsafe.Pointer(&res))
+	RegisterUnloadable(retval)
+
+	//Return the result
+	return retval, goFontChars
+}
+*/
+
 func LoadFontEx(fileName string, fontSize int, fontChars *int, charsCount int) *Font {
 	cfileName := C.CString(fileName)
 	//cFontChars := C.CString(fontChars)
@@ -116,6 +145,18 @@ func DrawTextRec(font Font, text string, rec Rectangle, fontSize float32, spacin
 	defer C.free(unsafe.Pointer(ctext))
 	cfont := *font.cptr()
 	C.DrawTextRec(cfont, ctext, crec, C.float(fontSize), C.float(spacing), C.bool(wordWrap), ctint)
+}
+
+//DrawTextRecEx : Draw text using font inside rectangle limits with support for text selection
+func DrawTextRecEx(font Font, text string, rec Rectangle, fontSize float32, spacing float32, wordWrap bool, tint Color, selectStart int, selectLength int, selectText Color, selectBack Color) {
+	cselectBack := *selectBack.cptr()
+	cselectText := *selectText.cptr()
+	ctint := *tint.cptr()
+	crec := *rec.cptr()
+	ctext := C.CString(text)
+	defer C.free(unsafe.Pointer(ctext))
+	cfont := *font.cptr()
+	C.DrawTextRecEx(cfont, ctext, crec, C.float(fontSize), C.float(spacing), C.bool(wordWrap), ctint, C.int(selectStart), C.int(selectLength), cselectText, cselectBack)
 }
 
 //DrawTextCodepoint : Draw one character (codepoint)
